9a956c221aa0c560ceb8df062331a35e000a831b
Bilal <bilalfarooq6001@gmail.com>
2024-06-06 22:51:25 +0500

    debug error. save messag not receving

--- events/messageCreate/saveMessage.js
+++ events/messageCreate/saveMessage.js
@@ -5,45 +5,52 @@ const data = require("./../../data.json");
 const { COUNTING_CHANNNEL_ID } = process.env;
 
 module.exports = async (message) => {
-  try {
-    const { channel, content, guild } = message;
+	try {
+		const { channel, content, guild } = message;
 
-    if (channel.id !== process.env.COUNTING_CHANNNEL_ID) return;
+		if (channel.id !== process.env.COUNTING_CHANNNEL_ID) return;
 
-    const personSaveUsed = content.toLowerCase().includes("your saves");
-    const guildSaveUsed =
-      content.toLowerCase().includes("guild saves") ||
-      content.toLowerCase().includes("ruined it at");
+		console.log("Message received in counting channel");
+		const personSaveUsed = content.toLowerCase().includes("your saves");
+		const guildSaveUsed =
+			content.toLowerCase().includes("guild saves") ||
+			content.toLowerCase().includes("ruined it at");
 
-    if (!personSaveUsed && !guildSaveUsed) return;
+		if (!personSaveUsed && !guildSaveUsed) return;
 
-    const referenceMessage = await isCorrectCountMessage(message);
+		console.log(`${new Date()}: this is a save usage message`);
 
-    if (!referenceMessage) return;
+		const referenceMessage = await isCorrectCountMessage(message);
 
-    const hasRole = referenceMessage.member.roles.cache.has(
-      data.configuration.COUNTING_ROLE_ID
-    );
+		if (!referenceMessage) return;
 
-    await referenceMessage.member.roles.remove(
-      data.configuration.COUNTING_ROLE_ID
-    );
-    if (!guildSaveUsed) return;
+		console.log("ofc a bot message");
 
-    // notify
+		const hasRole = referenceMessage.member.roles.cache.has(
+			data.configuration.COUNTING_ROLE_ID
+		);
 
-    await channel.permissionOverwrites.set([
-      ...channel.permissionOverwrites.cache.values(),
-      {
-        id: data.configuration.COUNTING_ROLE_ID,
-        deny: [
-          PermissionsBitField.Flags.SendMessages,
-          PermissionsBitField.Flags.ManageMessages,
-        ],
-      },
-    ]);
-    await sendLockChannelEmbed(referenceMessage.member, hasRole);
-  } catch (error) {
-    console.log(error);
-  }
+		await referenceMessage.member.roles.remove(
+			data.configuration.COUNTING_ROLE_ID
+		);
+		if (!guildSaveUsed) return;
+
+		console.log("removing perms");
+
+		// notify
+
+		await channel.permissionOverwrites.set([
+			...channel.permissionOverwrites.cache.values(),
+			{
+				id: data.configuration.COUNTING_ROLE_ID,
+				deny: [
+					PermissionsBitField.Flags.SendMessages,
+					PermissionsBitField.Flags.ManageMessages,
+				],
+			},
+		]);
+		await sendLockChannelEmbed(referenceMessage.member, hasRole);
+	} catch (error) {
+		console.log(error);
+	}
 };
