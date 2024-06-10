85dbe4e4d4bab40bee4635c30dd4511bd5a15f31
CalebUSA <29007834+CalebUSA@users.noreply.github.com>
2024-06-10 10:47:09 -0800

    Update saveMessage.js to add logging for guild saves
    Added logging if guild save is used.

--- events/messageCreate/saveMessage.js
+++ events/messageCreate/saveMessage.js
@@ -2,7 +2,7 @@ const { PermissionsBitField } = require("discord.js");
 const { isCorrectCountMessage } = require("../../utils/count");
 const { sendLockChannelEmbed } = require("../../utils/embed");
 const data = require("./../../data.json");
-const { COUNTING_CHANNNEL_ID, LOGS_CHANNEL_ID } = process.env;
+const { COUNTING_CHANNNEL_ID, LOGS_CHANNEL_ID, ADMIN_ROLE, MOD_ROLE } = process.env;
 
 module.exports = async (message) => {
 	try {
@@ -85,6 +85,16 @@ module.exports = async (message) => {
 				],
 			},
 		]);
+
+		const adminRole = guild.roles.cache.get(ADMIN_ROLE);
+		const modRole = guild.roles.cache.get(MOD_ROLE);
+		const guildSaveMessage = `${adminRole} ${modRole} ${user} used a guild save! I locked the channel from the counting role but please check the channel asap to see if someone is trying to ruin our count.`;
+
+		console.log(guildSaveMessage);
+		if (logChannel) {
+			logChannel.send(guildSaveMessage);
+		}
+
 		await sendLockChannelEmbed(referenceMessage.member, hasRole);
 	} catch (error) {
 		console.log(error);
