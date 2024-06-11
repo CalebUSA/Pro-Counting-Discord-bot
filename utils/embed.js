566978f433661ed408a2f4a9911cbd8a8e29a71f
CalebUSA <29007834+CalebUSA@users.noreply.github.com>
2024-06-10 21:28:05 -0800

    Update embed.js updated disclaimer footer

--- utils/embed.js
+++ utils/embed.js
@@ -3,63 +3,16 @@ const { VOTE_CHANNEL_ID, MAIN_BOT_SERVER_CHANNEL_ID } = process.env;
 const data = require("./../data.json");
 
 const generateNoMatchEmbed = function (user, needed) {
-  const text = `I'm sorry but you don't meet the counting requirements for Counting Cove. We require at least \`${
-    data.configuration.correctRate
-  } %\`, \`${data.configuration.correct} correctly counted\`, and \`${
-    data.configuration.saves
-  } saves\`. If you need more saves type c!vote in ⁠${channelMention(
-    VOTE_CHANNEL_ID
-  )}. If you need a higher count or percentage you can go to the main counting bot server (found in ⁠${channelMention(
-    MAIN_BOT_SERVER_CHANNEL_ID
-  )}) and raise your stats there then return when you meet the qualifications. Below you can see how much higher stats you need:\n\n**Higher percentage required:** ${needed.correctRate
-    .toFixed(3)
-    .toString()
-    .replace(
-      /(\.0+|0+)$/,
-      ""
-    )} %\n**Higher correct count required:** ${needed.correct
-    .toFixed(3)
-    .toString()
-    .replace(/(\.0+|0+)$/, "")}\n**Higher saves required:** ${needed.saves
-    .toFixed(3)
-    .toString()
-    .replace(/(\.0+|0+)$/, "")}`;
+  const text = `I'm sorry but you don't meet the counting requirements for Counting Cove. We require at least \`${data.configuration.correctRate} %\`, \`${data.configuration.correct} correctly counted\`, and \`${data.configuration.saves} saves\`. If you need more saves type c!vote in ${channelMention(VOTE_CHANNEL_ID)}. If you need a higher count or percentage you can go to the main counting bot server (found in ${channelMention(MAIN_BOT_SERVER_CHANNEL_ID)}) and raise your stats there then return when you meet the qualifications. Below you can see how much higher stats you need:\n\n**Higher percentage required:** ${needed.correctRate.toFixed(3).toString().replace(/(\.0+|0+)$/, "")} %\n**Higher correct count required:** ${needed.correct.toFixed(3).toString().replace(/(\.0+|0+)$/, "")}\n**Higher saves required:** ${needed.saves.toFixed(3).toString().replace(/(\.0+|0+)$/, "")}`;
+  
   const embed = new EmbedBuilder()
     .setColor(0x0099ff)
     .setTitle("Requirements Missing")
-
     .setDescription(text)
-    .setThumbnail(user.displayAvatarURL());
-
-  return embed;
-};
-
-const saveusedembedunder = function (user, needed) {
-  const text = `I'm sorry but since you made a mistake counting, you no longer meet the counting requirements for Counting Cove. We require at least \`${
-    data.configuration.correctRate
-  } %\`, \`${data.configuration.correct} correctly counted\`, and \`${
-    data.configuration.saves
-  } saves\`. To get more saves type c!vote in ⁠${channelMention(
-    VOTE_CHANNEL_ID
-  )}. Below you can see how much higher stats you need:\n\n**Higher percentage required:** ${needed.correctRate
-    .toFixed(3)
-    .toString()
-    .replace(
-      /(\.0+|0+)$/,
-      ""
-    )} %\n**Higher correct count required:** ${needed.correct
-    .toFixed(3)
-    .toString()
-    .replace(/(\.0+|0+)$/, "")}\n**Higher saves required:** ${needed.saves
-    .toFixed(3)
-    .toString()
-    .replace(/(\.0+|0+)$/, "")}`;
-  const embed = new EmbedBuilder()
-    .setColor(0x0099ff)
-    .setTitle("Requirements Missing")
-
-    .setDescription(text)
-    .setThumbnail(user.displayAvatarURL());
+    .setThumbnail(user.displayAvatarURL())
+    .setFooter({
+      text: "Please note: Whether you are granted access or not is ultimately up to the admins of the server and their decision is final."
+    });
 
   return embed;
 };
@@ -68,7 +21,6 @@ const sendReqNotifyEmbed = async (member) => {
   const embed = new EmbedBuilder()
     .setColor(0x0099ff)
     .setTitle("Counting Role Added 🔢")
-
     .setThumbnail(member.displayAvatarURL())
     .addFields({
       name: "User",
@@ -81,7 +33,6 @@ const sendLockChannelEmbed = async (member, hasRole) => {
   const embed = new EmbedBuilder()
     .setColor(0x0099ff)
     .setTitle("Channel locked 🚨")
-
     .setThumbnail(member.displayAvatarURL())
     .addFields(
       {
@@ -101,6 +52,7 @@ const sendEmbedLogs = async (guild, embed) => {
   const logsCh = guild.channels.cache.get(process.env.LOGS_CHANNEL_ID);
   await logsCh.send({ embeds: [embed] });
 };
+
 module.exports = {
   generateNoMatchEmbed,
   sendReqNotifyEmbed,
