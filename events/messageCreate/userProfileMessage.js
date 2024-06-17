b5088c00323be6b71f7268f8d29be2cd7fdf9e86
CalebUSA <29007834+CalebUSA@users.noreply.github.com>
2024-06-17 11:37:01 -0800

    Update userProfileMessage.js

--- events/messageCreate/userProfileMessage.js
+++ events/messageCreate/userProfileMessage.js
@@ -34,12 +34,13 @@ module.exports = async (message) => {
         if (hasRole) {
             const saves = parseFloat(globalStatField.value.match(/Saves:\s*(\d+(?:\.\d+)?)/)?.[1] || 0);
             console.log(`Saves value for ${member.displayName}: ${saves}`);
+            return;
 
             if (saves < 1.5) {
-//                await member.roles.remove(data.configuration.COUNTING_ROLE_ID);
+                await member.roles.remove(data.configuration.COUNTING_ROLE_ID);
                 await referenceMessage.react('❌');
-//                await message.channel.send({
-//                   content: `${member}, it looks like you are falling behind on saves. Please go to <#${VOTE_CHANNEL_ID}> and do \`c!vote\`. Once you get more saves, ask for perms to count again in <#${GENERAL_CHANNEL}>.`,
+                await message.channel.send({
+                   content: `${member}, it looks like you are falling behind on saves. Please go to <#${VOTE_CHANNEL_ID}> and do \`c!vote\`. Once you get more saves, ask for perms to count again in <#${GENERAL_CHANNEL}>.`,
                     allowedMentions: { users: [member.id] },
                 });
                 return; // Return here if saves < 1.5
