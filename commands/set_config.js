8201f9b4a3822a41f71d5f4c0c5eb608cbfa54a6
CalebUSA <29007834+CalebUSA@users.noreply.github.com>
2024-06-22 13:03:51 -0800

    Update set_config.js update

--- commands/set_config.js
+++ commands/set_config.js
@@ -2,73 +2,89 @@ const fs = require("fs");
 const path = require("path");
 const { CommandType } = require("wokcommands");
 const {
-  handleInteractionError,
-  replyOrEditInteraction,
+	handleInteractionError,
+	replyOrEditInteraction,
 } = require("../utils/interaction");
 
 const data = require("./../data.json");
 const { PermissionFlagsBits } = require("discord.js");
 
 module.exports = {
-  // Required for slash commands
-  description: "Setup configuration",
-  // Create a legacy and slash command
-  type: CommandType.SLASH,
-  //   permissions: [PermissionFlagsBits.Administrator],
+	// Required for slash commands
+	description: "Setup configuration",
+	// Create a legacy and slash command
+	type: CommandType.SLASH,
+	//   permissions: [PermissionFlagsBits.Administrator],
 
-  options: [
-    {
-      name: "correct_rate",
-      description: "correct rate in percentage",
-      type: 10,
-      required: true,
-    },
-    {
-      name: "correct",
-      description: "correct ✅",
-      type: 10,
-      required: true,
-    },
-    {
-      name: "saves",
-      description: "view all the licenses for a role",
-      type: 10,
-      required: true,
-    },
-    {
-      name: "count_role",
-      description: "role that will be given on stats complete",
-      type: 8,
-      required: true,
-    },
-  ],
-  callback: async ({ interaction }) => {
-    const { options } = interaction;
-    try {
-      await interaction.deferReply({ ephemeral: true });
+	options: [
+		{
+			name: "correct_rate",
+			description: "correct rate in percentage",
+			type: 10,
+			required: true,
+		},
+		{
+			name: "correct",
+			description: "correct ✅",
+			type: 10,
+			required: true,
+		},
+		{
+			name: "saves",
+			description: "saves",
+			type: 10,
+			required: true,
+		},
+		{
+			name: "count_role",
+			description: "role that will be given on stats complete",
+			type: 8,
+			required: true,
+		},
+		{
+			name: "vote_saves",
+			description: "set saves for vote command",
+			type: 10,
+			required: true,
+		},
+		{
+			name: "vote_save_role",
+			description: "set vote save role",
+			type: 8,
+			required: true,
+		},
+	],
+	callback: async ({ interaction }) => {
+		const { options } = interaction;
+		try {
+			await interaction.deferReply({ ephemeral: true });
 
-      const correctRate = options.getNumber("correct_rate");
-      const correct = options.getNumber("correct");
-      const saves = options.getNumber("saves");
-      const { id: COUNTING_ROLE_ID } = options.getRole("count_role");
+			const correctRate = options.getNumber("correct_rate");
+			const correct = options.getNumber("correct");
+			const saves = options.getNumber("saves");
+			const vote_saves = options.getNumber("vote_saves");
+			const { id: COUNTING_ROLE_ID } = options.getRole("count_role");
+			const { id: VOTE_SAVE_ROLE_ID } = options.getRole("vote_save_role");
 
-      data.configuration = {
-        correctRate,
-        correct,
-        saves,
-        COUNTING_ROLE_ID,
-      };
+			data.configuration = {
+				correctRate,
+				correct,
+				saves,
+				COUNTING_ROLE_ID,
+				vote_saves,
+				VOTE_SAVE_ROLE_ID,
+			};
 
-      fs.writeFileSync(
-        path.join(__dirname, "..", "data.json"),
-        JSON.stringify(data)
-      );
+			fs.writeFileSync(
+				path.join(__dirname, "..", "data.json"),
+				JSON.stringify(data)
+			);
 
-      await replyOrEditInteraction(interaction, {
-        content: "Changed the config!",
-      });
-    } catch (err) {
-      await handleInteractionError(err, interaction);
-    }
-  },
+			await replyOrEditInteraction(interaction, {
+				content: "Changed the config!",
+			});
+		} catch (err) {
+			await handleInteractionError(err, interaction);
+		}
+	},
 };
