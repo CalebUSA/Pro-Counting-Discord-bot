acf6bae5ea44e4c72be577a6b36490586488e1d6
CalebUSA <29007834+CalebUSA@users.noreply.github.com>
2024-06-22 13:02:34 -0800

    Update view_config.js undo

--- commands/view_config.js
+++ commands/view_config.js
@@ -1,31 +1,31 @@
 const { CommandType } = require("wokcommands");
 const {
-	handleInteractionError,
-	replyOrEditInteraction,
+  handleInteractionError,
+  replyOrEditInteraction,
 } = require("../utils/interaction");
 
 const data = require("./../data.json");
 const { PermissionFlagsBits } = require("discord.js");
 
 module.exports = {
-	// Required for slash commands
-	description: "View the configuration",
-	// Create a legacy and slash command
-	//   permissions: [PermissionFlagsBits.Administrator],
-	type: CommandType.SLASH,
+  // Required for slash commands
+  description: "View the configuration",
+  // Create a legacy and slash command
+  //   permissions: [PermissionFlagsBits.Administrator],
+  type: CommandType.SLASH,
 
-	callback: async ({ interaction }) => {
-		try {
-			const { configuration } = data;
+  callback: async ({ interaction }) => {
+    try {
+      const { configuration } = data;
 
-			const content = `**Correct Rate Percentage:** ${configuration.correctRate}\n**Correct ✅:** ${configuration.correct}\n**Saves:** ${configuration.saves}\n**Counting role**: <@&${configuration.COUNTING_ROLE_ID}>\n**Vote saves:** ${configuration.vote_saves}\n**Vote save role**: <@&${configuration.VOTE_SAVE_ROLE_ID}>`;
+      const content = `**Correct Rate Percentage:** ${configuration.correctRate}\n**Correct ✅:** ${configuration.correct}\n**Saves:** ${configuration.saves}\n**Counting role**: <@&${configuration.COUNTING_ROLE_ID}>`;
 
-			await replyOrEditInteraction(interaction, {
-				content,
-				ephemeral: true,
-			});
-		} catch (err) {
-			await handleInteractionError(err, interaction);
-		}
-	},
+      await replyOrEditInteraction(interaction, {
+        content,
+        ephemeral: true,
+      });
+    } catch (err) {
+      await handleInteractionError(err, interaction);
+    }
+  },
 };
